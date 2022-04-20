877c004aee7402c3a658bb2333c0657d1360b535
yfsmth <33195116+yfsmth@users.noreply.github.com>
2022-04-20 22:51:47 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,5 +10,11 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.1.5/g' package/base-files/files/bin/config_generate
+#1 Modify default IP
+sed -i 's/192.168.1.1/192.168.1.5/g' package/base-files/files/bin/config_generate
+
+#2. Clear the login password
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
+
+#3. Replace with JerryKuKu’s Argon
+rm openwrt/package/lean/luci-theme-argon -rf
